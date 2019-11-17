353f6ac456e94785c2e6a8040f0ceaf97763046a
Lyralei16 <44676382+Lyralei16@users.noreply.github.com>
2019-11-17 01:47:58 +0100

    folder structure changed

--- game/sheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="sheet" tilewidth="70" tileheight="70" tilecount="98" columns="14">
- <image source="sheet.png" trans="000000" width="980" height="490"/>
-</tileset>
--- game/tilemap/spritesheet_jumper.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="spritesheet_jumper" tilewidth="16" tileheight="16" tilecount="8192" columns="64">
- <image source="../jumperpack_kenney/Spritesheets/spritesheet_jumper.png" trans="000000" width="1024" height="2048"/>
-</tileset>
